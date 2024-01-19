90e404937bd970ff30d989317d4fe9774bf3cfc5
Eun Jin Park <ej.park385@gmail.com>
2024-01-19 16:48:54 +0900

    Add TestButton

--- app/src/main/java/com/example/goodjob/MainActivity.kt
+++ app/src/main/java/com/example/goodjob/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.goodjob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testButton = findViewById<Button>(R.id.button)
+        testButton.setOnClickListener {
+            Toast.makeText(this, "Test Button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
