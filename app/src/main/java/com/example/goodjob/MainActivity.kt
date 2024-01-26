6ed5ba900f05b90c2046e5931ebb4f1c60128d50
Eun Jin Park <ej.park385@gmail.com>
2024-01-26 17:34:56 +0900

    레이아웃 통합

--- app/src/main/java/com/example/goodjob/MainActivity.kt
+++ app/src/main/java/com/example/goodjob/MainActivity.kt
@@ -9,12 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val testButton = findViewById<Button>(R.id.button)
-        testButton.setOnClickListener {
-            Toast.makeText(this, "Test Button", Toast.LENGTH_SHORT).show()
-            // Register Activity Test
-            setContentView(R.layout.activity_signup)
-        }
     }
 }
